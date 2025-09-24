c5fecfd95f746fc09c57ac0b9b723ef3c01d6175
Ami Raj <ami.raj2307@gmail.com>
2025-09-24 00:09:56 -0400

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,12 @@
-def triangle(n):
+def triangle(n, total=None):
+    if total is None:
+        total = n  
     if n <= 0:
         return []
-    rows = triangle(n - 1)
-    stars = ' '.join(['*'] * n)  
-    row_str = ' ' * (n - 1) + stars
+    rows = triangle(n - 1, total)
+    stars = ' '.join(['*'] * n)
+    spaces = ' ' * (total - n)
+    row_str = spaces + stars
     return rows + [row_str]
 
 def show_triangle(n):
