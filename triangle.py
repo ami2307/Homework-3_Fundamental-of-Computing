75595bf7c326752583f272adae574e13890ee8f4
Ami Raj <ami.raj2307@gmail.com>
2025-09-24 00:07:22 -0400

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,10 @@
 def triangle(n):
     if n <= 0:
         return []
-    rows = triangle(n-1)
-    row_str = ' ' * (n-1) + '* ' * n
-    return rows + [row_str.rstrip()]
+    rows = triangle(n - 1)
+    stars = ' '.join(['*'] * n)  
+    row_str = ' ' * (n - 1) + stars
+    return rows + [row_str]
 
 def show_triangle(n):
     for line in triangle(n):
