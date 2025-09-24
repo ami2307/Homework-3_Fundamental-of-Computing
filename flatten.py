9c0c27ca972ea407ac583b6f22376e4c7e47f2cc
Ami Raj <ami.raj2307@gmail.com>
2025-09-23 23:28:48 -0400

    Create flatten.py

---
+++ flatten.py
@@ -0,0 +1,8 @@
+def flatten(nested):
+    flat = []
+    for item in nested:
+        if isinstance(item, list):
+            flat.extend(flatten(item))
+        else:
+            flat.append(item)
+    return flat
